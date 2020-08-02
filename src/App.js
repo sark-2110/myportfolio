e0eb533f693a9fd9c4ed3cd73b63b51899a06e95
sark2110 <khandelwalsarvesh8@gmail.com>
2020-08-02 10:49:45 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import Main from './component/Main';
+import Home from './component/Home';
+import Bio from './component/Bio';
+import Education from './component/Education';
+import Internship from './component/Internship';
+import Contact from './component/Contact';
+import Footer from './component/Footer';
+import myStyle from './css/myStyle.css';
 
 function App() {
   return (
-    <div className="App">
-      <Main />
+    <div className="container-fluid" style={myStyle}>
+        <Home />
+        <Bio />
+        <Education />
+        <Internship />
+        <Contact />
+        <Footer />
     </div>
   );
 }
